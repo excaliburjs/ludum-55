1e65a563db021b2d4ae178afbd9d0b456b6004ad
jedeen <jedeen@users.noreply.github.com>
2024-04-13 12:06:36 -0500

    Add basic puzzle numbers

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,30 @@
 export default {
     SoundVolume: 0.5,
     BackgroundAmbianceVolume: 0.5,
-    BackgroundMusicVolume: 0.5
+    BackgroundMusicVolume: 0.5,
+
+    puzzles: {
+        0: [
+            [2, 5],
+            [3, 1]
+        ],
+        1: [
+            [5, 2, 2],
+            [5, 3, 5],
+            [1, 5, 0]
+        ],
+        2: [
+            [3, 1, 1, 2],
+            [-5, 2, 1, 0],
+            [2, 1, 2, 5],
+            [0, 5, 2, -2]
+        ],
+        3: [
+            [2, 1, 5, 2, 5],
+            [1, 5, 3, 5, -2],
+            [-5, 2, 5, 0, 0],
+            [3, 1, 5, 3, 3],
+            [0, -2, -5, 1, 0]
+        ]
+    }
 }
