41f1dc7ebe10eea211939027684d6c88a35c7245
Jason Harris <Jasonlh.Harris@gmail.com>
2024-04-14 15:03:03 -0500

    Keyboard Mouse place fixed

--- src/levels/main-level.ts
+++ src/levels/main-level.ts
@@ -24,15 +24,15 @@ export class Level extends Scene {
   currentSelection: Unit | null = null;
   inventory!: Inventory;
   currentSelectedCoordinate: { x: number; y: number } = { x: 0, y: 0 };
-
+  
   constructor(private level: number) {
     super();
     this.puzzleGrid = buildPuzzle(level, this);
-
+    
     this.camera.zoom = 2;
     this.camera.pos = this.puzzleGrid.iso.transform.pos.add(vec(0, 100));
   }
-
+  
   onInitialize(engine: Engine<any>): void {
     SoundManager.startBackgroundMusic();
     this.backgroundColor = Color.Black;
@@ -53,7 +53,7 @@ export class Level extends Scene {
   onDeactivate(context: SceneActivationContext<undefined>): void {
     this.inventory.toggleVisible(false);
   }
-
+  
   moveSelection = (evt: PointerEvent) => {
     if (this.currentSelection) {
       this.currentSelection.pos = evt.worldPos;
@@ -64,12 +64,19 @@ export class Level extends Scene {
       this.currentSelectedCoordinate = { x: tile.x, y: tile.y };
     }
   };
-
+  
   placeSelectionOnTile = (x: number, y: number) => {
     if (this.currentSelection) {
-        const unitType = this.currentSelection.config.type;
-        const success = this.puzzleGrid.addUnit(
-          this.currentSelection, x, y);
+      
+      const previousUnit = this.puzzleGrid.getUnit(x, y);
+      if (!!previousUnit && !previousUnit.config.fixed) {
+        this.puzzleGrid.clearCell(x, y);
+        SfxrSounds.remove.play();
+      }
+      
+      const unitType = this.currentSelection.config.type;
+      const success = this.puzzleGrid.addUnit(
+        this.currentSelection, x, y);
         if (success) {
           this.currentSelection = null;
           this.checkSolution();
@@ -79,164 +86,164 @@ export class Level extends Scene {
           if (valueHint) {
             valueHint.graphics.use(ValueHintSprite[unitType]);
             unit?.actions.fade(0.4, 250).callMethod(() => {
-                valueHint.graphics.visible = true;
+              valueHint.graphics.visible = true;
             });
           }
-   
+          
           SfxrSounds.place.play();
         }
       }
-  }
-
-  placeUnitWithPointer = (evt: PointerEvent) => {
-    if (this.puzzleGrid.validTile(evt.worldPos)) {
-      const tileCoord = this.puzzleGrid.getTileCoord(evt.worldPos);
-      // TODO unify this logic so you can do it with keyboard also
-      if (tileCoord) {
-        const previousUnit = this.puzzleGrid.getUnit(tileCoord.x, tileCoord.y);
-        if (!!previousUnit && !previousUnit.config.fixed) {
-          this.puzzleGrid.clearCell(tileCoord.x, tileCoord.y);
-          SfxrSounds.remove.play();
+    }
+    
+    placeUnitWithPointer = (evt: PointerEvent) => {
+      if (this.puzzleGrid.validTile(evt.worldPos)) {
+        const tileCoord = this.puzzleGrid.getTileCoord(evt.worldPos);
+        // TODO unify this logic so you can do it with keyboard also
+        if (tileCoord) {
+          const previousUnit = this.puzzleGrid.getUnit(tileCoord.x, tileCoord.y);
+          this.placeSelectionOnTile(tileCoord.x, tileCoord.y);
+          if (!!previousUnit) {
+            this.selectUnit(previousUnit);
+          }
         }
-        this.placeSelectionOnTile(tileCoord.x, tileCoord.y);
-        if (!!previousUnit) {
-          this.selectUnit(previousUnit);
-        }
-      }
-    } else {
-      this.cancelSelection();
-      SfxrSounds.remove.play();
-    }
-  };
-
-  placeUnitWithKeyboard = () => {
-    this.placeSelectionOnTile(this.currentSelectedCoordinate.x, this.currentSelectedCoordinate.y)
-  };
-  clearCellWithKeyboard = () => {
-    let tileX = this.currentSelectedCoordinate.x;
-    let tileY = this.currentSelectedCoordinate.y;
-    const previousUnit = this.puzzleGrid.getUnit(tileX, tileY);
-    if(!!previousUnit) {
-      if(previousUnit.config.fixed){ return; }
-      this.puzzleGrid.clearCell(tileX, tileY);
-      this.inventory.addToInventory(previousUnit.config.type);
-    }
-  };
-
-  checkSolution() {
-    if (this.puzzleGrid.checkSolved()) {
-      const nextLevel = this.level + 1;
-      const sceneKey = `level ${nextLevel}`;
-      SfxrSounds.clearPuzzle.play();
-      this.engine.addScene(sceneKey, new Level(nextLevel));
-      this.engine.goToScene(sceneKey, {
-        destinationIn: new FadeInOut({ direction: "in", duration: 2000 }),
-        sourceOut: new FadeInOut({ direction: "out", duration: 2000 }),
-      });
-    }
-  }
-
-  keyboardDown = (evt: KeyEvent) => {
-    // Move cursor
-    switch (evt.key) {
-      case Keys.A:
-      case Keys.ArrowLeft: {
-        this.currentSelectedCoordinate.x = clamp(
-          this.currentSelectedCoordinate.x - 1,
-          0,
-          this.puzzleGrid.dimension - 1
-        );
-        break;
-      }
-      case Keys.D:
-      case Keys.ArrowRight: {
-        this.currentSelectedCoordinate.x = clamp(
-          this.currentSelectedCoordinate.x + 1,
-          0,
-          this.puzzleGrid.dimension - 1
-        );
-        break;
-      }
-      case Keys.W:
-      case Keys.ArrowUp: {
-        this.currentSelectedCoordinate.y = clamp(
-          this.currentSelectedCoordinate.y - 1,
-          0,
-          this.puzzleGrid.dimension - 1
-        );
-        break;
-      }
-      case Keys.S:
-      case Keys.ArrowDown: {
-        this.currentSelectedCoordinate.y = clamp(
-          this.currentSelectedCoordinate.y + 1,
-          0,
-          this.puzzleGrid.dimension - 1
-        );
-        break;
-      }
-    }
-    this.puzzleGrid.showHighlightByCoordinate(
-      this.currentSelectedCoordinate.x,
-      this.currentSelectedCoordinate.y
-    );
-
-    switch (evt.key) {
-      case Keys.Digit1:
-      case Keys.Numpad1: {
-        this.inventory.onSelection("rat")();
-        this.placeUnitWithKeyboard();
-        break;
-      }
-      case Keys.Digit2:
-      case Keys.Numpad2: {
-        this.inventory.onSelection("goblin")();
-        this.placeUnitWithKeyboard();
-        break;
-      }
-      case Keys.Digit3:
-      case Keys.Numpad3: {
-        this.inventory.onSelection("orc")();
-        this.placeUnitWithKeyboard();
-        break;
-      }
-      case Keys.Digit5:
-      case Keys.Numpad5: {
-        this.inventory.onSelection("dragon")();
-        this.placeUnitWithKeyboard();
-        break;
-      }
-      case Keys.Delete:
-      case Keys.Backspace:{
-        this.clearCellWithKeyboard();
-        break;
-      }
-      case Keys.Esc: {
+      } else {
         this.cancelSelection();
-        break;
+        SfxrSounds.remove.play();
+      }
+    };
+    
+    placeUnitWithKeyboard = () => {
+      const previousUnit = this.puzzleGrid.getUnit(this.currentSelectedCoordinate.x, this.currentSelectedCoordinate.y);
+      this.placeSelectionOnTile(this.currentSelectedCoordinate.x, this.currentSelectedCoordinate.y)
+      if(!!previousUnit && !previousUnit.config.fixed) this.inventory.addToInventory(previousUnit?.config.type);
+    };
+    
+    clearCellWithKeyboard = () => {
+      let tileX = this.currentSelectedCoordinate.x;
+      let tileY = this.currentSelectedCoordinate.y;
+      const previousUnit = this.puzzleGrid.getUnit(tileX, tileY);
+      if(!!previousUnit) {
+        if(previousUnit.config.fixed){ return; }
+        this.puzzleGrid.clearCell(tileX, tileY);
+        this.inventory.addToInventory(previousUnit.config.type);
+      }
+    };
+    
+    checkSolution() {
+      if (this.puzzleGrid.checkSolved()) {
+        const nextLevel = this.level + 1;
+        const sceneKey = `level ${nextLevel}`;
+        SfxrSounds.clearPuzzle.play();
+        this.engine.addScene(sceneKey, new Level(nextLevel));
+        this.engine.goToScene(sceneKey, {
+          destinationIn: new FadeInOut({ direction: "in", duration: 2000 }),
+          sourceOut: new FadeInOut({ direction: "out", duration: 2000 }),
+        });
       }
     }
-  };
-
-  selectUnit(unit: Unit) {
-    if (this.currentSelection) {
-      this.remove(this.currentSelection);
-      this.currentSelection = null;
+    
+    keyboardDown = (evt: KeyEvent) => {
+      // Move cursor
+      switch (evt.key) {
+        case Keys.A:
+        case Keys.ArrowLeft: {
+          this.currentSelectedCoordinate.x = clamp(
+            this.currentSelectedCoordinate.x - 1,
+            0,
+            this.puzzleGrid.dimension - 1
+          );
+          break;
+        }
+        case Keys.D:
+        case Keys.ArrowRight: {
+          this.currentSelectedCoordinate.x = clamp(
+            this.currentSelectedCoordinate.x + 1,
+            0,
+            this.puzzleGrid.dimension - 1
+          );
+          break;
+        }
+        case Keys.W:
+        case Keys.ArrowUp: {
+          this.currentSelectedCoordinate.y = clamp(
+            this.currentSelectedCoordinate.y - 1,
+            0,
+            this.puzzleGrid.dimension - 1
+          );
+          break;
+        }
+        case Keys.S:
+        case Keys.ArrowDown: {
+          this.currentSelectedCoordinate.y = clamp(
+            this.currentSelectedCoordinate.y + 1,
+            0,
+            this.puzzleGrid.dimension - 1
+          );
+          break;
+        }
+      }
+      this.puzzleGrid.showHighlightByCoordinate(
+        this.currentSelectedCoordinate.x,
+        this.currentSelectedCoordinate.y
+      );
+      
+      switch (evt.key) {
+        case Keys.Digit1:
+        case Keys.Numpad1: {
+          this.inventory.onSelection("rat")();
+          this.placeUnitWithKeyboard();
+          break;
+        }
+        case Keys.Digit2:
+        case Keys.Numpad2: {
+          this.inventory.onSelection("goblin")();
+          this.placeUnitWithKeyboard();
+          break;
+        }
+        case Keys.Digit3:
+        case Keys.Numpad3: {
+          this.inventory.onSelection("orc")();
+          this.placeUnitWithKeyboard();
+          break;
+        }
+        case Keys.Digit5:
+        case Keys.Numpad5: {
+          this.inventory.onSelection("dragon")();
+          this.placeUnitWithKeyboard();
+          break;
+        }
+        case Keys.Delete:
+        case Keys.Backspace:{
+          this.clearCellWithKeyboard();
+          break;
+        }
+        case Keys.Esc: {
+          this.cancelSelection();
+          break;
+        }
+      }
+    };
+    
+    selectUnit(unit: Unit) {
+      if (this.currentSelection) {
+        this.remove(this.currentSelection);
+        this.currentSelection = null;
+      }
+      unit.addComponent(new IsometricEntityComponent(this.puzzleGrid.iso));
+      unit.get(IsometricEntityComponent).elevation = 3;
+      this.currentSelection = unit;
+      this.add(unit);
     }
-    unit.addComponent(new IsometricEntityComponent(this.puzzleGrid.iso));
-    unit.get(IsometricEntityComponent).elevation = 3;
-    this.currentSelection = unit;
-    this.add(unit);
-  }
-
-  cancelSelection() {
-    if (this.currentSelection) {
-      this.remove(this.currentSelection);
-      const type = this.currentSelection.config.type;
-      const counts = this.inventory.getInventoryConfig();
-      counts[type]++;
-      this.inventory.setInventoryConfig(counts);
-      this.currentSelection = null;
+    
+    cancelSelection() {
+      if (this.currentSelection) {
+        this.remove(this.currentSelection);
+        const type = this.currentSelection.config.type;
+        const counts = this.inventory.getInventoryConfig();
+        counts[type]++;
+        this.inventory.setInventoryConfig(counts);
+        this.currentSelection = null;
+      }
     }
   }
-}
+  
